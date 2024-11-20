87d140373bbfca042acd46956888812346a1dc0d
moacirdavidag <moacirdavidag@gmail.com>
2024-11-20 13:59:46 -0300

    feat: Basic country infos on Country Page

--- src/pages/Country/Country.tsx
+++ src/pages/Country/Country.tsx
@@ -1,10 +1,124 @@
-import React from 'react'
-import { CountryContainer } from './style'
+import React, { useEffect, useState } from "react";
+import {
+    CountryBordersList,
+  CountryContainer,
+  CountryInfoWrapper,
+  Row,
+  TopBackComponent,
+} from "./style";
+import { useNavigate, useParams } from "react-router-dom";
+import API from "../../services/api";
+
+import arrowBackIcon from "../../assets/ico/arrow-back-icon.svg";
+import infoIconSVG from "../../assets/ico/info-circle.svg";
+
+type Country = {
+  commonName: string;
+  officialName: string;
+  countryCode: string;
+  region: string;
+  borders: [
+    {
+      commonName: string;
+      officialName: string;
+      countryCode: string;
+      region: string;
+    },
+  ];
+};
+
+type CountryFlag = {
+  name: string;
+  flag: string;
+  iso2: string;
+  iso3: string;
+};
 
 const Country = () => {
-  return (
-    <CountryContainer>Country</CountryContainer>
-  )
-}
+  const { code } = useParams();
+  const [countryInfo, setCountryInfo] = useState<Partial<Country>>({});
+  const [countryPopulation, setCountryPopulation] = useState<
+    Partial<CountryFlag>
+  >({});
+  const [countryFlagURL, setCountryFlagURL] = useState<Partial<CountryFlag>>(
+    {}
+  );
 
-export default Country
+  const navigate = useNavigate();
+
+  const handleNavigateBack = () => {
+    navigate("/");
+  };
+
+  const handleNavigateToCountryPage = (code: string) => {
+    navigate(`/country/${code}`);
+  };
+
+  const getCountryInfo = async () => {
+    try {
+      await API.get(`/countries/${code}`).then((response) => {
+        if (response.status === 200) {
+          setCountryInfo(response.data);
+        }
+      });
+      await API.get(`/countries/flag/${code}`).then((response) => {
+        if (response.status === 200) {
+          setCountryFlagURL(response.data);
+        }
+      });
+      await API.get(``);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getCountryInfo();
+  }, [code]);
+  return (
+    <CountryContainer>
+      <TopBackComponent>
+        <img src={arrowBackIcon} alt="Back" onClick={handleNavigateBack} /> Back
+      </TopBackComponent>
+      <CountryInfoWrapper>
+        <Row $rowDirection="row" $gap={24}>
+          <div>
+            <img
+              src={countryFlagURL.flag}
+              alt={countryFlagURL.name}
+              className="country-flag"
+            />
+          </div>
+
+          <Row $rowDirection="column" $gap={12}>
+            <span className="country-name">{countryInfo.commonName}</span>
+            <p><span className="bold-span">Official Name:</span> {countryInfo.officialName}</p>
+            <p><span className="bold-span">Region:</span> {countryInfo.region}</p>
+          </Row>
+        </Row>
+        <Row>
+            <span className='bold-span'>Country borders:</span>
+        </Row>
+        <CountryBordersList>
+        {countryInfo.borders && countryInfo.borders?.length > 0 ? (
+          countryInfo.borders.map((country) => (
+            <div>
+              <span>{country.commonName}</span>
+              <span>
+                {" "}
+                <img src={infoIconSVG} alt="Country info icon" onClick={() => {
+                  handleNavigateToCountryPage(country.countryCode)
+                }} />
+              </span>
+            </div>
+          ))
+        ) : (
+          <p>No countries has border with {countryInfo.commonName}</p>
+        )}
+        </CountryBordersList>
+      </CountryInfoWrapper>
+    </CountryContainer>
+  );
+};
+
+export default Country;
--- src/pages/Country/style.ts
+++ src/pages/Country/style.ts
@@ -1,5 +1,10 @@
 import styled from "styled-components";
 
+type RowProps = {
+  $rowDirection?: string;
+  $gap?: number;
+};
+
 export const CountryContainer = styled.main`
   width: 100%;
   height: 100vh;
@@ -7,4 +12,71 @@ export const CountryContainer = styled.main`
   align-items: center;
   justify-content: center;
   flex-direction: column;
+  .bold-span {
+    font-weight: 600;
+  }
+`;
+
+export const TopBackComponent = styled.div`
+  width: 50%;
+  padding: 8px 24px;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  cursor: pointer;
+  font-weight: 600;
+  img {
+    width: 30px;
+    height: auto;
+  }
+`;
+
+export const CountryInfoWrapper = styled.div`
+  width: 50%;
+  margin-top: 24px;
+  padding: 8px 24px;
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  border: 1px solid #cecece;
+`;
+
+export const Row = styled.div<RowProps>`
+  width: 100%;
+  display: flex;
+  flex-direction: ${({ $rowDirection }) =>
+    $rowDirection === "row" ? "row" : "column"};
+  gap: 6px;
+  .country-flag {
+    max-width: 150px;
+    height: auto;
+  }
+  .country-name {
+    font-size: 24px;
+    font-weight: 800;
+  }
+`;
+
+export const CountryBordersList = styled.div`
+  width: 100%;
+  min-height: 20px;
+  border: 1px solid #d3d3d3;
+  border-radius: 8px;
+  margin-top: 12px;
+  padding: 12px;
+  overflow-y: scroll;
+  div {
+    margin: 4px;
+    display: flex;
+    justify-content: space-between;
+    &:hover {
+        background-color: #eeeeee;
+        transition: 300ms ease-in-out;
+    }
+    span img {
+        width: 20px;
+        height: auto;
+        cursor: pointer;
+    }
+  }
 `;
