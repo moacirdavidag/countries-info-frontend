4bfffc6b650b80d8f8144f0d4eb2aaa9711c1a81
moacirdavidag <moacirdavidag@gmail.com>
2024-11-20 12:53:36 -0300

    view: Home layout defined

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,13 +2,22 @@ import React, { useEffect, useState } from "react";
 import {
   CountriesListWrapper,
   HomeContainer,
+  HomeFooter,
   SearchCountryInput,
+  SearchInputDivWrapper,
 } from "./style";
 import API from "../../services/api";
+import searchIconSVG from "../../assets/ico/search-ico.svg";
+import infoIconSVG from "../../assets/ico/info-circle.svg";
+
+type Country = {
+  name: string;
+  code: string;
+};
 
 const Home = () => {
-  const [countries, setCountries] = useState([]);
-  const [countriesFiltered, setCountriesFiltered] = useState([]);
+  const [countries, setCountries] = useState<Country[]>([]);
+  const [countriesFiltered, setCountriesFiltered] = useState<Country[]>([]);
   const [inputSearchValue, setInputSearchValue] = useState<string>("");
 
   const handleChangeInputSearchValue = (value: string) => {
@@ -17,34 +26,54 @@ const Home = () => {
 
   const getCountries = async () => {
     try {
-      await API.get('/countries')
-      .then((response) => {
-        if(response.status === 200) {
+      await API.get("/countries").then((response) => {
+        if (response.status === 200) {
           setCountries(response.data);
         }
-      })
+      });
     } catch (error) {
       console.error(error);
     }
-  }
+  };
 
   useEffect(() => {
     getCountries();
-  }, [])
+  }, []);
 
   return (
     <HomeContainer>
       <h1>Countries</h1>
       <h2>Search by your country info :)</h2>
-      <SearchCountryInput
-        placeholder="Type your country name..."
-        value={inputSearchValue}
-        onChange={(event) => {
-          const value = event.target.value;
-          handleChangeInputSearchValue(value);
-        }}
-      />
-      <CountriesListWrapper />
+      <SearchInputDivWrapper>
+        <SearchCountryInput
+          placeholder="Type your country name..."
+          value={inputSearchValue}
+          onChange={(event) => {
+            const value = event.target.value;
+            handleChangeInputSearchValue(value);
+          }}
+        />
+        <img src={searchIconSVG} alt="Search icon" />
+      </SearchInputDivWrapper>
+
+      <CountriesListWrapper>
+        {countries && countries?.length > 0 ? (
+          countries.map((country) => (
+            <div>
+              <span>{country.name}</span>
+              <span>
+                {" "}
+                <img src={infoIconSVG} alt="Country info icon" />
+              </span>
+            </div>
+          ))
+        ) : (
+          <p>No country finded.</p>
+        )}
+      </CountriesListWrapper>
+      <HomeFooter>
+        Developed with &#10084; by <a href='https://www.linkedin.com/in/moacir-david-7735b7158/' target="_blank">Moacir David</a>
+      </HomeFooter>
     </HomeContainer>
   );
 };
--- src/pages/Home/style.ts
+++ src/pages/Home/style.ts
@@ -1,30 +1,65 @@
 import styled from "styled-components";
 
 export const HomeContainer = styled.main`
-    width: 100%;
-    height: 100vh;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+export const SearchInputDivWrapper = styled.div`
+  width: 300px;
+  height: 40px;
+  position: relative;
+  margin-top: 24px;
+  img {
+    position: absolute;
+    right: 10px;
+    top: 10px;
+    width: 20px;
+    height: auto;
+  }
 `;
 
 export const SearchCountryInput = styled.input`
-    width: 300px;
-    height: 40px;
-    padding: 10px;
-    border-radius: 8px;
-    border: 1px solid #d3d3d3;
-    outline: none;
-    margin-top: 24px;
+  width: 100%;
+  height: auto;
+  padding: 10px;
+  border-radius: 8px;
+  border: 1px solid #d3d3d3;
+  outline: none;
 `;
 
 export const CountriesListWrapper = styled.div`
-    width: 30%;
-    min-height: 60px;
-    border: 1px solid #d3d3d3;
-    border-radius: 8px;
+  width: 30%;
+  min-height: 60px;
+  height: 100px;
+  border: 1px solid #d3d3d3;
+  border-radius: 8px;
+  margin-top: 24px;
+  padding: 12px;
+  overflow-y: scroll;
+  div {
+    margin: 4px;
+    display: flex;
+    justify-content: space-between;
+    &:hover {
+        background-color: #eeeeee;
+        transition: 300ms ease-in-out;
+    }
+    span img {
+        width: 20px;
+        height: auto;
+        cursor: pointer;
+    }
+  }
+`;
+
+export const HomeFooter = styled.footer`
+    width: 100%;
+    text-align: center;
+    color: #808080;
     margin-top: 24px;
-    padding: 8px;
-    overflow-y: scroll;
 `;
